b34183bedc7b46fa8cb09f0b27947ea1c7d6dd71
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-03-01 12:26:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //mongoose.connect('mongodb://localhost:27017/test-db')
-mongoose.connect('mongodb+srv://user:1234@db.mhbax.mongodb.net/test-db?retryWrites=true&w=majority'
+mongoose.connect('mongodb+srv://@db.mhbax.mongodb.net/test-db?retryWrites=true&w=majority'
 ,{ useNewUrlParser: true, useUnifiedTopology:true})
     .then(()=> console.log('Mongo is UP'))
     .catch((err) => console.log('Mongo is down. Raison :',err));
